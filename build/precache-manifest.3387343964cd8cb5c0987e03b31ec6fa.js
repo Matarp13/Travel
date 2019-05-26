0bbe1baf49c6d5a0706525034ee135e7dddc6bbf
Adir Sation <adirush120@gmail.com>
2019-05-26 17:34:31 +0300

    Adding build

---
+++ build/precache-manifest.3387343964cd8cb5c0987e03b31ec6fa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba77d7c72d250e327c2d84a8c0c29b38",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f1cc334ec8fb0fb8c84e",
+    "url": "/static/css/main.b826b538.chunk.css"
+  },
+  {
+    "revision": "0dd0d6f60f84d06852a7",
+    "url": "/static/js/2.6fc3fffc.chunk.js"
+  },
+  {
+    "revision": "f1cc334ec8fb0fb8c84e",
+    "url": "/static/js/main.6827cbd3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f06e90b8dd8cd9065239ed8b6d548916",
+    "url": "/static/media/logo.f06e90b8.png"
+  }
+]);
